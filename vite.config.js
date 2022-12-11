40210cc0efa551964e11d7697d4064d1fc27daee
nemerem-x <pilotprecious@gmail.com>
2022-12-11 22:34:07 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,17 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}
-  }
+  },
+  build: {
+    chunkSizeWarningLimit:1500,
+    rollupOptions: {
+        output:{
+            manualChunks(id) {
+                if (id.includes('node_modules')) {
+                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                }
+            }
+        }
+    }
+}
 })
